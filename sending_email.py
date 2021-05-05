78d9329db11249133e111006a3fae2c321abf683
Luís Augusto <83483411+Radamantus@users.noreply.github.com>
2021-05-04 21:43:40 -0300

    Update sending_email.py

--- sending_email.py
+++ sending_email.py
@@ -1,9 +1,9 @@
 import win32com.client as win32
 
-# criar a integração com o outlook
+# Criar a integração com o outlook
 outlook = win32.Dispatch('outlook.application')
 
-# criar um email
+# Criar um e-mail
 email = outlook.CreateItem(0)
 
 faturamento = 1500
@@ -11,20 +11,21 @@ qtde_produtos = 10
 ticket_medio = faturamento / qtde_produtos
 
 # configurar as informações do seu e-mail
-email.To = "pythonimpressionador@gmail.com; pythonimpressionador+lira@gmail.com"
-email.Subject = "E-mail automático do Python"
+# email.To = "fulano@gmail.com; beltrano.email@gmail.com"
+email.To = "sicrano@gmail.com"
+email.Subject = "E-mail Automático do Python"
 email.HTMLBody = f"""
-<p>Olá Lira, aqui é o código Python</p>
+<p>Olá pessoa, aqui é o código Python</p>
 
 <p>O faturamento da loja foi de R${faturamento}</p>
 <p>Vendemos {qtde_produtos} produtos</p>
 <p>O ticket Médio foi de R${ticket_medio}</p>
 
-<p>Abs,</p>
+<p>Cordialmente,</p>
 <p>Código Python</p>
 """
 
-# anexo = "C://Users/joaop/Downloads/arquivo.xlsx"
+# anexo = "C://Users/luism/Documents/MATLAB/example_file.txt"
 # email.Attachments.Add(anexo)
 
 email.Send()
