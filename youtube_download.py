f41a9b7e3b54e26d429bcb6247d32f645fb9aafe
Luís Augusto <83483411+Radamantus@users.noreply.github.com>
2021-06-14 21:31:43 -0300

    Add files via upload

---
+++ youtube_download.py
@@ -0,0 +1,12 @@
+# Importar biblioteca
+from pytube import YouTube
+
+# Cria objeto com URL do vídeo a ser baixado
+video = YouTube('https://www.youtube.com/watch?v=gF2qYW1hIVk')
+
+# Configura a melhor qualidade de vídeo
+stream = video.streams.get_highest_resolution()
+
+# Faz o download do vídeo selecioando e o salva na pasta especificada do computador
+folder = r'/Users/luism/Documents/MATLAB/'
+stream.download(output_path = folder)
