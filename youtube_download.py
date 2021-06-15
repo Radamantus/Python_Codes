eeb7ad3ab4d9d5e175f3fe460e0312ca90494e36
Luís Augusto <83483411+Radamantus@users.noreply.github.com>
2021-06-14 21:32:09 -0300

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -7,6 +7,6 @@ video = YouTube('https://www.youtube.com/watch?v=gF2qYW1hIVk')
 # Configura a melhor qualidade de vídeo
 stream = video.streams.get_highest_resolution()
 
-# Faz o download do vídeo selecioando e o salva na pasta especificada do computador
+# Faz o download do vídeo selecionado e o salva na pasta especificada do computador
 folder = r'/Users/luism/Documents/MATLAB/'
 stream.download(output_path = folder)
